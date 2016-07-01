295fb9bc94cbec0ab572838dcb69ae0ce783aa89
nerocool996 <hanand1996@gmail.com>
2016-07-01 12:46:41 +0000

    uploading to heroku

--- database_setup.py
+++ database_setup.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String 
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime 
 
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -13,20 +13,19 @@ import urlparse
 
 Base = declarative_base()
 ##Config Ends##
-class Links (Base):
-	__tablename__= 'links'
-	url = Column(String(300),nullable= False)
+class Queries (Base):
+	__tablename__= 'query'
+	query = Column(String(300),nullable= False)
 	id = Column(Integer, primary_key = True)
-	short_URL = Column(String(300),nullable= False)
+	time = Column(DateTime,nullable= False)
 	@property
 	def serialize(self):
 		return {
-		'URL' : self.url,
-		'id' : self.id,
-		'shortURL' : self.short_URL
+		'query' : self.query,
+		'time' : self.time
 		}
 	
 ##END OF FILE##
-engine = create_engine('postgres://eftouahahwepdz:L1bqXJw6TrrHoASeNh8XmjpgRB@ec2-54-243-212-72.compute-1.amazonaws.com:5432/d56424prqdm2cu')
+engine = create_engine('postgres://jmmodrydkyhole:HOD93bvzChb_zpa30S1ScrHvsE@ec2-54-221-235-135.compute-1.amazonaws.com:5432/d323kk8ffr81ad')
 
 Base.metadata.create_all(engine)
--- finalproject.py
+++ finalproject.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for,flash, jsonify,make_response
 from datetime import date,timedelta
-from database_setup import Base,Links
+from database_setup import Base,Queries
 app = Flask(__name__)
 
 from sqlalchemy import create_engine,desc,func
@@ -8,7 +8,14 @@ from sqlalchemy.orm import sessionmaker
 from urlparse import urlparse
 from random import random
 
-engine = create_engine('postgres://eftouahahwepdz:L1bqXJw6TrrHoASeNh8XmjpgRB@ec2-54-243-212-72.compute-1.amazonaws.com:5432/d56424prqdm2cu')
+import httplib, urllib, base64,json,datetime
+
+headers = {
+    # Request headers
+    'Ocp-Apim-Subscription-Key': '3f7ef1515220435eaa80d7da5e1b5239',
+}
+
+engine = create_engine('postgres://jmmodrydkyhole:HOD93bvzChb_zpa30S1ScrHvsE@ec2-54-221-235-135.compute-1.amazonaws.com:5432/d323kk8ffr81ad')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
@@ -19,36 +26,40 @@ def mainpage():
 	return render_template('main.html')
 
 
-
+@app.route('/latest')
+def latest():
+	allElement = session.query(Queries).order_by(Queries.time).all()
+	return jsonify(latest=[i.serialize for i in allElement])
         
-@app.route('/new/<path:url>')
+@app.route('/imagesearch/<string:url>')
 def TimeStamp(url):
-	Purl = 	urlparse(url)	
-	if (Purl.netloc):
-		st,en = 97,122
-		sURL = ""
-		
-		for i in range(0,4):
-			ch= 26*random()+97
-			sURL += chr(int(ch))
-		newURL = Links(url=url,short_URL=sURL)
-		session.add(newURL)
-		session.commit()
-		return jsonify({"URL":url,"Short URL":sURL})
-	else:
-		return jsonify({"URL":url,"Short URL":"Not a valid url"})
+	try:
+		searchcount = request.args.get('offset', '10')
+		print (url, searchcount)
+		params = urllib.urlencode({
+    			# Request parameters
+  	  		'q': 	url,
+  	  		'count': searchcount,
+  	  		'offset': '0',
+  	  		'mkt': 'en-us',
+  	  		'safeSearch': 'Moderate',
+		})
+		print params
+		conn = httplib.HTTPSConnection('api.cognitive.microsoft.com')
+	 	conn.request("GET", "/bing/v5.0/images/search?%s" % params, "{body}", headers)
+		responses = conn.getresponse()
+	  	##Converts a string to Dictionary object 
+	  	data =  json.loads(responses.read())
+	 	conn.close()
+	 	new = Queries(query=url,time = datetime.datetime.now())
+	 	session.add(new)
+	 	session.commit()
+	  	res = {"result":data["value"]}
+  	  	
+	   	return jsonify(res)
+	except:
+		return jsonify({"value":"Error"})
 	
-@app.route('/<string:url>')
-def redir(url):
-	if (len(url)==4):
-		try:
-			res = session.query(Links).filter_by(short_URL =url).one()
-			return redirect(res.url)
-		except:
-			return jsonify({"short_URL":"Does not exist in data base"})
-	else:
-		return jsonify({"short_URL":"Does not exist in data base"})
-
 ##if __name__ == '__main__':
 app.secret_key = 'uberBlack'
 app.debug = True
